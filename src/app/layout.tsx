f36100738f813553a112527b82734412ed39db93
lukmanc405 <lukmanc405@gmail.com>
2024-01-22 05:25:12 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ import { ReactNode } from "react";
 import ThemeRegister from "@/components/ThemeRegister";
 
 export const metadata: Metadata = {
-  title: "Polaris - Automatic Alpha Script",
-  description: "Automatic Alpha Script",
+  title: "lukeTech - Automatic EVM Script",
+  description: "Automatic EVM Script",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
